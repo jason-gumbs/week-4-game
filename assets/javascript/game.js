c7dfe604a035e7f2c57df9f07cc427b84ded3925
Will <fwgumbs@gmail.com>
2017-12-14 16:37:32 -0500

    new

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+$(".characters").click(function(){
+  $(".characters1").hide();
+  $(".characters2").hide();
+  $(".characters3").hide();
+  $("p").hide();
+  $("p").hide();
+  $("p").hide();
+});
+$(".characters1").click(function(){
+    $("input:text").val("Glenn Quagmire");
+});
+$(".characters2").click(function(){
+    $("input:text").val("Glenn Quagmire");
+});
+$(".characters3").click(function(){
+    $("input:text").val("Glenn Quagmire");
+});
