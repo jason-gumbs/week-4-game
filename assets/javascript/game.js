4c101f7ff70f09b3d5b7d9b054e098c9260f8b9a
Will <fwgumbs@gmail.com>
2017-12-16 09:01:14 -0500

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,88 @@
+$(".selectdefender").hide();
+$(".selectdefender1").hide();
+$(".selectdefender2").hide();
+$(".selectdefender3").hide();
+$(".defender").hide();
+$(".defender1").hide();
+$(".defender2").hide();
+$(".defender3").hide();
+
+ 
+
+
+///your character click functions
+
 $(".characters").click(function(){
   $(".characters1").hide();
   $(".characters2").hide();
   $(".characters3").hide();
-  $("p").hide();
-  $("p").hide();
-  $("p").hide();
+  $(".selectdefender1").show();
+$(".selectdefender2").show();
+$(".selectdefender3").show();
+  $(".select").hide();
+
 });
+
 $(".characters1").click(function(){
-    $("input:text").val("Glenn Quagmire");
+  $(".characters").hide();
+  $(".characters2").hide();
+  $(".characters3").hide();
+  $(".selectdefender").show();
+$(".selectdefender2").show();
+$(".selectdefender3").show();
+  $(".select").hide();
+
 });
 $(".characters2").click(function(){
-    $("input:text").val("Glenn Quagmire");
+  $(".characters1").hide();
+  $(".characters").hide();
+  $(".characters3").hide();
+  $(".selectdefender1").show();
+$(".selectdefender").show();
+$(".selectdefender3").show();
+  $(".select").hide();
+
 });
 $(".characters3").click(function(){
-    $("input:text").val("Glenn Quagmire");
+  $(".characters1").hide();
+  $(".characters2").hide();
+  $(".characters").hide();
+  $(".selectdefender1").show();
+$(".selectdefender2").show();
+$(".selectdefender").show();
+  $(".select").hide();
+
+});
+
+///select your enemy click fuctions Below
+$(".selectdefender").click(function(){
+  $(".selectdefender1").hide();
+  $(".selectdefender3").hide();
+  $(".selectdefender2").hide();
+    $(".selectdefender").hide();
+  $(".defender").show();
+
+ 
+
+});
+$(".selectdefender2").click(function(){
+  $(".selectdefender1").hide();
+  $(".selectdefender3").hide();
+  $(".selectdefender2").hide();
+    $(".selectdefender").hide();
+  $(".defender2").show();
+});
+$(".selectdefender3").click(function(){
+  $(".selectdefender1").hide();
+  $(".selectdefender3").hide();
+  $(".selectdefender2").hide();
+    $(".selectdefender").hide();
+  $(".defender3").show();
+});
+$(".selectdefender1").click(function(){
+  $(".selectdefender1").hide();
+  $(".selectdefender3").hide();
+  $(".selectdefender2").hide();
+    $(".selectdefender").hide();
+  $(".defender1").show();
 });
